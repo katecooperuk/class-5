f6800fb5eca1a48f6f7714a6572478b71ec8e2f9
Kate Cooper <kate.cooper@mac.com>
2013-10-09 10:32:23 +0100

    Updating PHP pages with code - video part 1 of 3

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+		<title>Killerphp.com - OOP in PHP</title>
+		
+		<?php include("class_lib.php"); ?>
+		
+	</head>
+	<body>
+		
+	</body>
+</html>
