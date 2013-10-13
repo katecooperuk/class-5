65cb0ec1ff6e0a9244e672e4cb762c7b5e641c5e
Kate Cooper <kate.cooper@mac.com>
2013-10-13 08:35:15 +0100

    Step 13 / 14: Constructors

--- class_lib.php
+++ class_lib.php
@@ -1,6 +1,13 @@
 <?php 
-	class person {
-		var $name;
+	// create the class of 'person'
+	// variable inside a class are called properties
+	// functions inside a class are called methods
+	// A class's method is used to manipulate its own data/properties
+	// getter and setter names should match the property: get_name / set_name
+	
+	// this is a built in variable
+	/*	class person {
+			var $name;
 		
 		function set_name($new_name) {
 			$this->name = $new_name;
@@ -10,6 +17,28 @@
 			return $this->name;
 		}
 	}	
-
+	*/
+	
+	// Constructors - initialize your object's properties (give your properties values)
+	class person {
+		var $name;
+		
+		function __construct($persons_name) {
+			$this->name = $persons_name;
+		}
+	
+		function set_name($new_name) {
+			$this->name = $new_name;
+		}
+		
+		function get_name() {
+			return $this->name;
+		}
+	}
+	
+	// Functions = Methods
+	// Variables = Properties
+	
+	
 
 ?>
--- index.php
+++ index.php
@@ -11,17 +11,29 @@
 	<body>
 		
 		<?php
-		
+			// 11. Accessing an Object's Data 
+			// instatiate a class - create an instance of it
+			// to create an object out of a class you use the 'new' keyword
+			/*
+			
 			$stefan = new person();
 			$nick = new person();
 			
+			// set properties using methods (setters) we created
 			$stefan->set_name("Stefan Mischook");
 			$nick->set_name("Nick Waddles");
 			
+			// use getter methods to access data held in our objects using (->)
 			echo "Stefan's full name: " . $stefan->get_name();
 			echo "Nick's full name: " . $nick->get_name();
 				
-		
+			*/
+			
+			// 14. Constructors
+			
+				$stefan = new person("Stefan Mischook");
+				
+				echo "Stefan's full name: " . $stefan->get_name();
 		?>
 		
 		
