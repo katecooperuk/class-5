0169cec49eb017d3076b92bfab4f3ae4132c9f04
Kate Cooper <kate.cooper@mac.com>
2013-10-13 09:50:49 +0100

    Step 21: Overriding Methods: Part 1

--- class_lib.php
+++ class_lib.php
@@ -2,6 +2,7 @@
 
 	// 'extends' is the keyword that enables inheritance
 	
+	/*
 	class employee extends person {
 		
 		function __construct($employee_name){
@@ -10,5 +11,49 @@
 		}
 		
 	}
+	*/
+	
+	// explicitily adding class properties are optional - but good practice
+	
+	class person {
+	
+		var $name;
+		
+			function __construct($persons_name) {
+				$this->name = $persons_name;
+			}
+			
+			function get_name() {
+				return $this->name;
+			}
+			
+			// protected methods and properties restrict access to those elements
+			
+			protected function set_name($new_name) {
+				if (name != "Jimmy Two Guns") {
+					$this->name = strtoupper($new_name);
+				}
+			}
+			
+	}
+	
+	// extends is the keyword that enables inheritance
+	
+	class employee extends person {
+		
+		protected function set_name($new_name) {
+			if ($new_name == "Stefan Lamp") {
+				$this->name = $new_name;
+			}
+		}
+		
+		function __construct($employee_name) {
+			$this->set_name($employee_name);
+		}
+	}
+	
+	// notice how set_name() is different in the 'employee' class from the version found in the parent class: 'person'
+	
+?>
 	
 	
