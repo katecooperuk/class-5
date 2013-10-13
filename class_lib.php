986af0b9e8cf66d41f0dd7c068dab65bc3d74cdd
Kate Cooper <kate.cooper@mac.com>
2013-10-13 09:30:07 +0100

    Step 19: Reusing Code with inheritance

--- class_lib.php
+++ class_lib.php
@@ -2,4 +2,13 @@
 
 	// 'extends' is the keyword that enables inheritance
 	
+	class employee extends person {
+		
+		function __construct($employee_name){
+			
+			$this->set_name($employee_name);
+		}
+		
+	}
+	
 	
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+		<title>Killerphp.com - OOP in PHP</title>
+		
+		<?php include("class_lib.php"); ?>
+		
+	</head>
+	
+	<body>
+		
+
+		
+		
+	</body>
+	
+</html>
