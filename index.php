41286a0eaf7a46a47dbf4ad7c6ed1249ae55908e
Kate Cooper <kate.cooper@mac.com>
2013-10-13 09:33:56 +0100

    Step 20: Reusing Code with Inheritance - Part 3

--- index.php
+++ index.php
@@ -10,7 +10,15 @@
 	
 	<body>
 		
-
+		<?php
+			// Using our PHP objects in our PHP pages.
+			$stefan = new person("Stefan Mischook");
+			echo "Stefan's full name: " . $stefan->get_name();
+			
+			$james = new employee("Johnny Fingers");
+			echo "---> " . $james->get_name();
+		
+		?>
 		
 		
 	</body>
